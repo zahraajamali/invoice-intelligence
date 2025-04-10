b04c774afec3e235efb0c0a1fea1bb7e9755139a
zahra jamali <zahraajamali79@gmail.com>
2025-04-10 10:00:00 +0330

    feat: complete neo4j config

--- main.py
+++ main.py
@@ -29,27 +29,27 @@ if __name__ == "__main__":
         convert_file_to_text(url,output_dir,api_token)
 
         # Step 3 : extract invoice data
-        # invoice_data= extract_invoice_data_from_gpt(openai_api_key,f"{output_dir}/page1.txt")
-        # print("invoice_data...",invoice_data)
+        invoice_data= extract_invoice_data_from_gpt(openai_api_key,f"{output_dir}/page1.txt")
+        print("invoice_data...",invoice_data)
 
-        # if(invoice_data):
-        #     # Step 4 : enrich Invoice with Neo4j
-        #     neo4j = Neo4jService(uri="bolt://localhost:7687", username="neo4j", password="del-ai123")
+        # Step 4 : enrich Invoice with Neo4j
+        if(invoice_data):
+            neo4j = Neo4jService(uri="bolt://localhost:7687", username="neo4j", password="del-ai123")
 
-        #     clients = neo4j.load_clients_from_json("datasets/inventory.suppliers.json")
-        #     items = neo4j.load_items_from_json("datasets/inventory.products.json")
-        #     neo4j.create_clients(clients)
-        #     neo4j.create_items(items)
+            clients = neo4j.load_clients_from_json("datasets/inventory.suppliers.json")
+            items = neo4j.load_items_from_json("datasets/inventory.products.json")
+            neo4j.create_clients(clients)
+            neo4j.create_items(items)
 
 
-        #     enriched_invoice = neo4j.enrich_invoice(invoice_data)
+            enriched_invoice = neo4j.enrich_invoice(invoice_data)
 
-        #     with open(f"{final_result_dir}/enriched_{file_name}.json", "w", encoding="utf-8") as f:
-        #         json.dump(enriched_invoice, f, indent=2, ensure_ascii=False)
+            with open(f"{final_result_dir}/enriched_{file_name}.json", "w", encoding="utf-8") as f:
+                json.dump(enriched_invoice, f, indent=2, ensure_ascii=False)
 
-        #     neo4j.close()
-        # else:
-        #     print('No invoice data extracted. Skipping enrichment step.')
+            neo4j.close()
+        else:
+            print('No invoice data extracted. Skipping enrichment step.')
     
     except Exception as e:
         print("An error occurred during invoice data extraction from GPT.")
